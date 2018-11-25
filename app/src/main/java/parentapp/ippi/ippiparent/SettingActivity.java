3a80ad341f2bdb372a56952db87557ac6ecd4cb6
nadia halim <37141440+nrlnadia@users.noreply.github.com>
2018-11-25 12:50:09 +0800

    change setting ui

--- app/src/main/java/parentapp/ippi/ippiparent/SettingActivity.java
+++ app/src/main/java/parentapp/ippi/ippiparent/SettingActivity.java
@@ -2,11 +2,14 @@ package parentapp.ippi.ippiparent;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,7 +20,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class SettingActivity extends AppCompatActivity {
 
-    private Button UpdateEmail, UpdatePassword, UpdateLocation, DeleteAccount;
+    private LinearLayout UpdateEmail, UpdatePassword, UpdateLocation, DeleteAccount;
     private FirebaseAuth mAuth;
     private FirebaseDatabase database;
 
@@ -28,12 +31,11 @@ public class SettingActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        UpdateEmail = findViewById(R.id.btChangeEmail);
-        UpdatePassword = findViewById(R.id.btChangePassword);
-        UpdateLocation = findViewById(R.id.btChangeLocation);
-        DeleteAccount = findViewById(R.id.btDeleteAccount);
+        UpdateEmail =findViewById(R.id.ChangeEmail) ;
+        UpdatePassword = findViewById(R.id.ChangePass);
+        UpdateLocation = findViewById(R.id.ChangeLocation);
+        DeleteAccount = findViewById(R.id.DeleteAccount);
         mAuth = FirebaseAuth.getInstance();
-        //hello
 
         //get current user
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
